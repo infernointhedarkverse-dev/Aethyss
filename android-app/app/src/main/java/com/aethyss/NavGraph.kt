30b0eff37c339684f8c00cbe684405fa58c7d8cb
infernointhedarkverse-dev <infernointhedarkverse@gmail.com>
2026-02-03 21:51:05 +0530

    Update NavGraph.kt

--- android-app/app/src/main/java/com/aethyss/NavGraph.kt
+++ android-app/app/src/main/java/com/aethyss/NavGraph.kt
@@ -2,8 +2,9 @@ package com.aethyss.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavType
-import androidx.navigation.compose.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.aethyss.ui.ChatScreen
 import com.aethyss.ui.HomeScreen
 
@@ -13,16 +14,9 @@ private const val ROUTE_CHAT = "chat"
 @Composable
 fun NavGraph(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
-
-    NavHost(
-        navController = navController,
-        startDestination = ROUTE_HOME,
-        modifier = modifier
-    ) {
+    NavHost(navController = navController, startDestination = ROUTE_HOME, modifier = modifier) {
         composable(ROUTE_HOME) {
-            HomeScreen(onOpenChat = {
-                navController.navigate(ROUTE_CHAT)
-            })
+            HomeScreen(onOpenChat = { navController.navigate(ROUTE_CHAT) })
         }
         composable(ROUTE_CHAT) {
             ChatScreen()
