3a1a8be0ba9501fea99752ac78fa1adc1b1b52dd
infernointhedarkverse-dev <infernointhedarkverse@gmail.com>
2026-02-03 23:46:42 +0530

    Refactor NavGraph function for improved readability

--- android-app/app/src/main/java/com/aethyss/navigation/NavGraph.kt
+++ android-app/app/src/main/java/com/aethyss/navigation/NavGraph.kt
@@ -12,12 +12,24 @@ private const val ROUTE_HOME = "home"
 private const val ROUTE_CHAT = "chat"
 
 @Composable
-fun NavGraph(modifier: Modifier = Modifier) {
+fun NavGraph(
+    modifier: Modifier = Modifier
+) {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = ROUTE_HOME, modifier = modifier) {
+
+    NavHost(
+        navController = navController,
+        startDestination = ROUTE_HOME,
+        modifier = modifier
+    ) {
         composable(ROUTE_HOME) {
-            HomeScreen(onOpenChat = { navController.navigate(ROUTE_CHAT) })
+            HomeScreen(
+                onOpenChat = {
+                    navController.navigate(ROUTE_CHAT)
+                }
+            )
         }
+
         composable(ROUTE_CHAT) {
             ChatScreen()
         }
